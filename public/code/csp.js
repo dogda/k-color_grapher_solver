82649990314fb856b392c0de225ca97443ca559e
Kaleb Gaar <kaleb.gaar@gmail.com>
2018-04-05 12:52:38 -0500

    added domain

--- public/code/csp.js
+++ public/code/csp.js
@@ -1,6 +1,6 @@
 module.exports = {
-  createCSP: createCSP
-}
+  createCSPFromFile: createCSPFromFile
+};
 
 var xml2js = require('xml2js');
 var fs = require('fs');
@@ -8,15 +8,14 @@ var fs = require('fs');
 function Variable(name, domain){
   this.name = name;
 
-  //Need better domain
-  this.domain = domain;
-  console.log(name);
+  this.originalDomain = domain;
+  this.currentDomain = this.originalDomain.clone();
 
   this.toString = function(){
     var result = "Name: "+ this.name + ", Domain: ";
-    for(var i = 0; i < this.domain.length; i++){
-      result = result + this.domain[i];
-      if(i != this.domain.length - 1) {
+    for(var i = 0; i < this.currentDomain.values.length; i++){
+      result = result + this.currentDomain.values[i];
+      if(i != this.currentDomain.values.length - 1) {
         result = result + ", ";
       }
     }
@@ -24,16 +23,39 @@ function Variable(name, domain){
   }
 }
 
+function Domain(name, values){
+  this.name = name;
+  this.values = values;
+
+  this.clone = function(){
+    return JSON.parse(JSON.stringify(this));
+  };
+
+  this.remove = function(value){
+    var result = [];
+    var count = 0;
+
+    for(var i = 0; i < this.values.length; i++){
+      if(this.values[i] != value){
+        result[count] = this.values[i];
+        count++;
+      }
+    }
+
+    return result;
+  };
+}
+
 function DiffConstraint(name, scope){
   this.name = name;
   this.scope = scope;
 
-  this.inScope = function(variableName){
-    if(scope[0] == variableName || scope[1] == variableName){
-      return true;
-    } else {
-      return false;
-    }
+  this.inScope = function(variableName) {
+    return (scope[0] == variableName || scope[1] == variableName);
+  };
+
+  this.toString = function() {
+    return "Name: " + this.name + ", Scope: " + this.scope[0] + ", " + this.scope[1];
   }
 }
 
@@ -49,35 +71,37 @@ function CSP(name, variables, constraints){
 
     var edge = this.constraints.find(function(e){
       return (e.inScope(a) && e.inScope(b));
-    })
+    });
 
-    if(typeof edge != 'undefined'){
-      return true;
-    } else {
-      return false;
-    }
+    return typeof edge != 'undefined';
   }
 }
 
-function createCSP(file){
+function createCSPFromFile(file){
 
   var parser = xml2js.Parser();
 
-  var variables = []
-  var constraints = []
+  var variables = [];
+  var constraints = [];
 
   fs.readFile(file, function(err, data){
     parser.parseString(data, function(err,result){
-      console.log(result.instance.variables[0].variable);
 
       for(var i = 0; i < result.instance.variables[0].variable.length; i++){
-        var domain = [1,2,3];
+        //Make Actual Domain
+        var domain = new Domain("Temp",[1,2,3]);
         variables[i] = new Variable(result.instance.variables[0].variable[i]['$'].name, domain);
         console.log(variables[i].toString());
+      }
+
+      for(var i = 0; i < result.instance.constraints[0].constraint.length; i++){
+        var scope = result.instance.constraints[0].constraint[i]['$'].scope.split(" ");
+        constraints[i] = new DiffConstraint(result.instance.constraints[0].constraint[i]['$'].name, scope);
+        //console.log(constraints[i].toString());
       }
     })
   })
 
 }
 
-createCSP("./testProblems/ColK4-conflicts.xml");
+createCSPFromFile("./testProblems/ColK4-conflicts.xml");
